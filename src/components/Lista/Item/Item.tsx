cf0333b6247984fb246a51ca4d2f7d05a8eaf136
abigailtechnology <116679728+abigailtechnology@users.noreply.github.com>
2023-12-05 15:08:13 -0300

    feat: select task

--- src/components/Lista/Item/Item.tsx
+++ src/components/Lista/Item/Item.tsx
@@ -1,15 +1,41 @@
-import style from  '../Lista.module.scss'
-import { ITarefa } from '../../../types/ITarefa';
+import style from "../Lista.module.scss";
+import { ITarefa } from "../../../types/ITarefa";
 
-function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
-    console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
-
-    return (
-        <li className={style.item}>
-            <h3>{tarefa}</h3>
-            <span>{tempo}</span>
-        </li>
-    )
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
 }
 
-export default Item
+function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id,
+  selecionaTarefa,
+}: Props) {
+  return (
+    <li
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ""} ${
+        completado ? style.itemCompletado : ""
+      }`}
+      onClick={() =>
+        !completado &&
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id,
+        })
+      }
+    >
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+      {completado && (
+        <span className={style.concluido} aria-label="tarefa completada"></span>
+      )}
+    </li>
+  );
+}
+
+export default Item;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,20 +1,49 @@
 import React, { useState } from 'react';
+import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
+import { ITarefa } from '../types/ITarefa';
 import style from './App.module.scss';
-import Cronometro from '../components/Cronometro';
 
 function App() {
-
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
+  }
 
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
-      <Cronometro />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
+      <Cronometro
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
